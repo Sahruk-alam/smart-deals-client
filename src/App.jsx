66e8a73a7159677ad407f377eb4eaed5c978d9a4
Sahruk alam sarder <sahrukalam8059@gmail.com>
2026-03-23 12:37:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import './index.css'
+function App() {
+  return (
+    <>
+      
+<h2 className='text-center text-4xl'>Welcome to React</h2>
+    </>
+  )
+}
+
+export default App
